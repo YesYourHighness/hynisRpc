77496bc544233ffd50154be4a27585e565f9baff
YesYourHighness <1046467756@qq.com>
2022-02-21 21:38:25 +0800

    Build Project

---
+++ hynisRpc/src/main/java/cn/yesmylord/hynis/HynisRpcApplication.java
@@ -0,0 +1,13 @@
+package cn.yesmylord.hynis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HynisRpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HynisRpcApplication.class, args);
+    }
+
+}
---
+++ hynisRpc/src/test/java/cn/yesmylord/hynis/HynisRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.yesmylord.hynis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HynisRpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
